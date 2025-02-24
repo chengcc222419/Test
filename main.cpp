92e7f2cfab14d306f843b2feee5abec22c5c1ba7
小王 <205556706@qq.com>
2025-02-24 09:56:52 +0800

    修改sort算法为快排算法

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 using namespace std;
 
+void sort(int arr[], int i, int j)
+{
+	int val = arr[i];
+	int l = i;
+	int r = j;
+	while(l < r)
+	{
+		while(l < r && arr[r] >= val) r --;
+		if(l < r)
+		{
+			arr[l ++] = arr[r];
+		}
+		while(l < r &&arr[l] <= val) l ++;
+		if(l < r)
+		{
+			arr[r --] = arr[l];
+		}
+		arr[l] = val;
+		sort(arr, i, l - 1);
+		sort(arr, l + 1, j);
+ 	}
+}
+
 //小陆添加的函数注释
 void sort(int arr[], int size)
 {
-	int tmp = 0;
-	bool flag  = false;
-	for(int i = 0; i < size - 1; i ++)
-	{
-		for(int j = 0; j < size - 1 - i; j ++)
-		{
-			if(arr[j] < arr[j + 1])
-			{
-				//小王修改排序算法 从大到小排序
-				tmp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = tmp;
-				flag == true;
-			}
-
-		}
-		if(!flag)
-			break;
-	}	
+	sort(arr, 0, size);
 }
 	
 int main()
@@ -31,5 +36,9 @@ int main()
 	int arr[] = {12, 4, 89, 67, 42};
 	int size = sizeof(arr) / sizeof(arr[0]);
 	sort(arr, size);
+	for(int i = 0; i < size; i ++)
+	{
+		cout << arr[i] << " ";
+	}
 	return 0;
 }
